07b4a37c56a3ca576c3c72d051806015a3bb4426
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-21 12:32:29 +0200

    defined strcuture for handling api call service

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,7 +6,33 @@
 
 <script>
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+    components: {
+
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        callServieApi () {
+            axios.get('/user', {
+                params: {
+                  ID: 12345
+                }
+            })
+            .then(function (response) {
+              console.log(response);
+            })
+            .catch(function (error) {
+              console.log(error);
+            })
+            .finally(function () {
+              // always executed
+            });  
+        }
+    },
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import { reactive } from "vue";
 
 // Store App Global Varibales
 export const store = reactive ({
-
+    apiKey: '',
+    apiCall: '',
 })
