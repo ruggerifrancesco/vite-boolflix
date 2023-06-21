863d9524e8ce2a937c720b290c7cf66cba6cc5a4
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-21 16:57:45 +0200

    added a new api link for tv series

--- src/store.js
+++ src/store.js
@@ -3,5 +3,6 @@ import { reactive } from "vue";
 // Store App Global Varibales
 export const store = reactive ({
     apiKey: 'cf6403cfc0feef64c52d0997cf3c00c4',
-    apiLinkCall: 'https://api.themoviedb.org/3/search/movie',
+    movieApiLinkCall: 'https://api.themoviedb.org/3/search/movie',
+    tvSeriesApiLinkCall: 'https://api.themoviedb.org/3/search/tv',
 })
