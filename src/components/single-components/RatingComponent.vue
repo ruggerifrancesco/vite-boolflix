d6a4988a954d08cf3221a4354d9cc40dae500eb5
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-22 16:05:03 +0200

    fgixed importing of single iccon, added 2 stars

--- src/components/single-components/RatingComponent.vue
+++ src/components/single-components/RatingComponent.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <font-awesome-icon icon="fa-solid fa-star" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['far', 'star']" />
     </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'RatingComponent',
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,15 +4,14 @@ import App from './App.vue'
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret)
+library.add(faStar, farStar)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
