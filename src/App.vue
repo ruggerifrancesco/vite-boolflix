af19fa54feec63b118289374f32541930d5f664d
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-22 17:37:51 +0200

    removed script not needing now

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,6 @@
   <router-view></router-view>
 </template>
 
-<script>
-import AppLogin from './components/AppLogin.vue';
-
-export default {
-  name: 'App',
-  components: {
-    AppLogin,
-  }
-}
-</script>
-
 <style lang="scss">
 @import "bootstrap/scss/bootstrap";
 </style>
