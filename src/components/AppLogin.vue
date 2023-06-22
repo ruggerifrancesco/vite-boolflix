ae7146f63491baa385c03c81e995106d9d11f53a
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-22 17:50:04 +0200

    renamed file to more simple name, addded first styles

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <!-- Your login form content -->
+  <main>
 
-    <button @click="redirectToAppWrapper">Go to App</button>
-  </div>
+    <div class="overlay-shadow">
+        <button @click="redirectToAppWrapper">Go to App</button>
+    </div>
+
+  </main>
 </template>
 
 <script>
@@ -16,6 +18,21 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    main {
+        display: block;
+        height: 100vh;
+        background-image: url('../assets/login-background.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+    }
 
+    .overlay-shadow {
+        height: 100%;
+        width: 100%;
+        background: rgb(0,0,0);
+        background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,212,255,0) 70%);    
+        overflow: hidden;
+    }
 </style>
