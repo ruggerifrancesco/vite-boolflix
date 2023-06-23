8fbedf0fad9424cdba1ebc8eba176445d73b2dbe
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-23 12:49:55 +0200

    fixed indentation, added shadow inset ofr more visibility

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,47 +1,54 @@
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg p-3">
-          <div class="container-fluid">
-            <div class="logo-brand">
-                <img src="../assets/boolflix-logo.png" alt="BoolFlix Logo">
+        <nav class="navbar navbar-expand-lg p-3 overlay-nav-shadow">
+            <div class="container-fluid">
+                <div class="logo-brand">
+                    <img src="../assets/boolflix-logo.png" alt="BoolFlix Logo">
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse list-items-container" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">
+                              Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Movies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tv Series</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">New Popular</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">My List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Browse By Language</a>
+                        </li>
+                    
+                    </ul>
+
+                    <div class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="searchQuery">
+                        <button class="btn btn-success" type="submit" @click="$emit('search', searchQuery)">Search</button>
+                    </div>
+                </div>
             </div>
-            
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse list-items-container" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Movies</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Tv Series</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">New Popular</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">My List</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Browse By Language</a>
-                </li>
-
-              </ul>
-              <div class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="searchQuery">
-                <button class="btn btn-outline-success" type="submit" @click="$emit('search', searchQuery)">Search</button>
-              </div>
-            </div>
-          </div>
         </nav>
+
+        <section class="hero">
+
+
+
+        </section>
 
     </header>
 </template>
@@ -66,6 +73,15 @@ export default {
 
     nav {
         background-color: transparent;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
+    .overlay-nav-shadow {
+        background: rgb(0,0,0);
+        background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,212,255,0) 100%);
     }
 
     img {
@@ -94,5 +110,13 @@ export default {
     // TO RECHECK LATER
     .list-items-container {
         margin-left: 1rem;
+    }
+
+    .hero {
+        height: 850px;
+        background-image: url('https://i.ytimg.com/vi_webp/JFf8I_8Ubv4/maxresdefault.webp');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: top;
     }
 </style>
