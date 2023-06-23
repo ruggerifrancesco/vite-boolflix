d8cfc081a9677c8c8c48be81bf804f88ff5bfdd4
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-23 11:34:31 +0200

    imported roboto, style all login container

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -10,7 +10,29 @@
                     <img src="../assets/boolflix-logo.png" alt="Main logo">
                   </div>
 
-                  <button @click="redirectToAppWrapper">Go to App</button>
+                  <form>
+                    <h1>Sign In</h1>
+
+                    <div class="mb-3">
+                      <label for="emailInput" class="form-label floating-label">Email address</label>
+                      <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" required>
+
+                      <span id="emailHelp">*We'll never share your email with anyone else.</span>
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="passwordInput" class="form-label floating-label">Password</label>
+                      <input type="password" class="form-control" id="passwordInput" required>
+                    </div>
+
+                    <div class="mb-3 form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+
+                    <button @click="redirectToAppWrapper" class="btn btn-custom">Go to App</button>
+                  </form>
+
                 </div>
 
             </div>
@@ -35,6 +57,14 @@ export default {
 @use '../styles/partials/_mixins.scss' as *;
 @use '../styles/partials/_variables.scss' as *;
 
+    * {
+      // outline: 1px solid red;
+    }
+
+    body {
+      font-family: "Roboto";
+    }
+
     main {
         display: block;
         height: 100vh;
@@ -42,12 +72,22 @@ export default {
         background-repeat: no-repeat;
         background-size: cover;
         background-position: center;
+        color: white;
     }
 
     img {
       display: block;
       width: 100%;
       height: 100%;
+    }
+
+    form {
+      width: 90%;
+    }
+
+    h1 {
+      align-self: flex-start;
+      margin-bottom: 2rem;
     }
 
     .overlay-shadow {
@@ -60,7 +100,6 @@ export default {
     }
 
     .login-container {
-        height: 800px;
         /* From https://css.glass */
         background: rgba(0, 0, 0, 0.15);
         border-radius: 16px;
@@ -68,9 +107,41 @@ export default {
         backdrop-filter: blur(11px);
         -webkit-backdrop-filter: blur(11px);
         @include flex (column, flex-start, center);
+        padding: 4rem 2rem 6rem 2rem;
     }
 
     .logo-container {
       width: 50%;
+      margin-bottom: 2rem;
     }
+
+    #emailInput, #passwordInput {
+      background-color: #444;
+      color: white;
+      border-radius: 0.2rem;
+      border: 1px solid #444;
+      height: 40px;
+    }
+
+    #emailHelp {
+      margin-top: 0.5rem;
+    }
+
+    input:focus {
+      box-shadow: 0 0 0 0.25rem rgba(253, 117, 13, 0.25);
+    }
+
+    .btn-custom {
+      width: 100%;
+      height: 50px;
+      margin-top: 24px;
+      cursor: pointer;
+      font-size: 18px;
+      font-weight: 700;
+      border-radius: 5px;
+      border: none;
+      color: #ffffff;
+      background-color: #e50914;
+    }
+    
 </style>
