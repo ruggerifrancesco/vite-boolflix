a8426c90a8f0c6bbd88c392f12817e35bfe4009e
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-23 11:59:10 +0200

    refactoring nav contents to match knock off netflix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,34 +1,39 @@
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg bg-light p-3">
           <div class="container-fluid">
-            <img src="../assets/boolflix-logo.png" alt="BoolFlix Logo">
+            <div class="logo-brand">
+                <img src="../assets/boolflix-logo.png" alt="BoolFlix Logo">
+            </div>
+            
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Home
                   </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link disabled">Disabled</a>
+                  <a class="nav-link" href="#">Movies</a>
                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Tv Series</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">New Popular</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">My List</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Browse By Language</a>
+                </li>
+
               </ul>
               <div class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="searchQuery">
@@ -56,5 +61,13 @@ export default {
 
 
 <style lang="scss" scoped>
-    
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+
+    .logo-brand {
+        width: 8rem;
+    }
 </style>
