feebc9b520f434ce03293e281e898cf63289cccf
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-23 11:50:50 +0200

    defined new nav bar structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,43 @@
 <template>
     <header>
-        <img src="../assets/boolflix-logo.png" alt="BoolFlix Logo">
-        <div class="d-flex">
-            <input class="form-control me-2 w-50" type="search" v-model="searchQuery" 
-                placeholder="Search" aria-label="Search">
-
-            <button class="btn btn-outline-success" type="submit" @click="$emit('search', searchQuery)">
-                Search
+        <nav class="navbar navbar-expand-lg bg-light">
+          <div class="container-fluid">
+            <img src="../assets/boolflix-logo.png" alt="BoolFlix Logo">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
             </button>
-        </div>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+              <div class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="searchQuery">
+                <button class="btn btn-outline-success" type="submit" @click="$emit('search', searchQuery)">Search</button>
+              </div>
+            </div>
+          </div>
+        </nav>
+
     </header>
 </template>
 
